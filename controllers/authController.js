467b8809f6d0816dcb9a2f33032b7653e84ef020
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-18 14:30:57 +0200

    updated controllers

---
+++ controllers/authController.js
@@ -0,0 +1,3 @@
+const { auth } = require("../config/firebase");
+
+exports.update = async (req, res) => {};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRouter");
 
 const app = express();
 app.use(cors());
