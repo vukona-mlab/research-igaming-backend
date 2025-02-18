599cd33539642d8c39147d1e3d0b5c9b0c8ecede
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-18 13:47:25 +0200

    initial commit

---
+++ config/firebase.js
@@ -0,0 +1,26 @@
+const firebase = require("firebase-admin");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+// Construct service account object from environment variables
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle escaped newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+});
+
+const auth = firebase.auth();
+
+module.exports = { auth };
