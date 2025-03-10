0ba9e50c05dfb6e2f932d67400378503bd71810f
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-10 14:50:24 +0200

    updated active status sockets

--- index.js
+++ index.js
@@ -7,12 +7,49 @@ const serviceRoutes = require("./routes/servicesRouter");
 const freelancerRoutes = require("./routes/freelancerRoutes");
 const testimonialRoutes = require("./routes/testimonialRoutes");
 const chatsRoutes = require("./routes/chatsRoutes");
+
+const { firebaseDb } = require("./config/firebase");
+
 const passport = require("passport");
+const socketIo = require("socket.io");
+const http = require("http");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  },
+});
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("active-status-update", async (data) => {
+    try {
+      await firebaseDb.collection("users").doc(data.uid).update({
+        activeStatus: data.activeStatus,
+        updatedAt: new Date(),
+      });
+
+      //get all chats
+
+      io.emit("get-active-status", {
+        uid: data.uid,
+        activeStatus: data.activeStatus,
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+});
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -28,6 +65,6 @@ app.use("/api", chatsRoutes);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
