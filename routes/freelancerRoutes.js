1038873e0a578bf70386e75229fb8595a91cc9b9
Khensani-Lebese <Khensanilebese0@gmail.com>
2025-03-14 13:09:55 +0200

    Added pagination for fetching freelancers

--- routes/freelancerRoutes.js
+++ routes/freelancerRoutes.js
@@ -5,9 +5,7 @@ const freelancerController = require("../controllers/freelancerController");
 // Route to get freelancers and their projects
 router.get("/freelancers/projects", freelancerController.getFreelancerProjects);
 
-// Route to get all projects
-router.get("/projects", freelancerController.getAllProjects);
-
+// Route to get freelancers
 router.get("/freelancers", freelancerController.getFreelancers);
 
 module.exports = router;
