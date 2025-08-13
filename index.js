ee7ddaf233c1341417378e7a2c739c678ab7f1d8
vukona-mlab <vukona@mlab.co.za>
2025-08-13 12:34:52 +0200

    added new origins to whitelist

--- index.js
+++ index.js
@@ -37,8 +37,11 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "http://localhost:5175",
       "http://localhost:3000",
       "http://localhost:3001",
+      "https://research-igaming.web.app/",
+      "https://admin-research-igaming.web.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
